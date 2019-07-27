20cb9b30eca9f6b1805a68f26d427bcc46cf0990
mahloko <mahloko.m2@gmail.com>
2019-07-27 09:34:03 +0200

    I think am bored now

---
+++ src/main/java/za/co/wethinkcode/app/App.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
