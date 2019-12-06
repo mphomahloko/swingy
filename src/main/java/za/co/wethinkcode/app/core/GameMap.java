97215bfbab7199d31c7c7c79fd685d891e65ca06
Mpho <43447189+Mahloko@users.noreply.github.com>
2019-12-06 16:26:21 +0200

    adding the enermy I think

--- src/main/java/za/co/wethinkcode/app/core/GameMap.java
+++ src/main/java/za/co/wethinkcode/app/core/GameMap.java
@@ -4,35 +4,35 @@ import za.co.wethinkcode.app.model.Hero;
 import za.co.wethinkcode.app.view.SwingyView;
 
 public class GameMap {
-	public String [][] map;
+	public String[][] map;
 	public SwingyView view;
 	public Hero hero;
 	public boolean gameState;
 
 	public GameMap(Hero hero, SwingyView view) {
 		this.hero = hero;
-		//rough sketch
+		// rough sketch
 		this.map = new String[20][20];
 		this.view = view;
 		_renderMap();
-		return ;
+		return;
 	}
-	
+
 	private void _renderMap() {
 		for (int i = 0; i < this.map.length; i += 1) {
 			for (int j = 0; j < this.map.length; j += 1) {
 				this.map[i][j] = ".";
 			}
 		}
+		this.map[6][10] = "E";
 		placePlayer();
-		return ;
+		return;
 	}
 
 	// Algorithm to fight I guess
 	public void placePlayer() {
-		if (this.map.length <= hero.getHeroY() || this.map[0].length <= hero.getHeroX()
-			|| hero.getHeroY() < 0 || hero.getHeroX() < 0)
-		{
+		if (this.map.length <= hero.getHeroY() || this.map[0].length <= hero.getHeroX() || hero.getHeroY() < 0
+				|| hero.getHeroX() < 0) {
 			gameState = false;
 			view.alertMsg("You Win");
 			hero.setHeroX(20 / 2);
@@ -40,37 +40,39 @@ public class GameMap {
 			placePlayer();
 			view.clearView();
 			view.iniView();
+		} else if (this.map[hero.getHeroY()][hero.getHeroX()] == "E") {
+			view.alertMsg("You have Encounterd an enermy");
 		} else {
 			this.map[hero.getHeroY()][hero.getHeroX()] = "P";
 		}
-		return ;
+		return;
 	}
 
 	public void moveRight() {
 		hero.setHeroX(hero.getHeroX() + 1);
 		_renderMap();
 		view.drawMap(map);
-		return ;
+		return;
 	}
 
 	public void moveLeft() {
 		hero.setHeroX(hero.getHeroX() - 1);
 		_renderMap();
 		view.drawMap(map);
-		return ;
+		return;
 	}
 
 	public void moveDown() {
 		hero.setHeroY(hero.getHeroY() + 1);
 		_renderMap();
 		view.drawMap(map);
-		return ;
+		return;
 	}
 
 	public void moveUp() {
 		hero.setHeroY(hero.getHeroY() - 1);
 		_renderMap();
 		view.drawMap(map);
-		return ;
+		return;
 	}
 }
